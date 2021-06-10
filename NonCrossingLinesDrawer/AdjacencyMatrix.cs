cec99968b1f7c5a14a4908f89645ef0538ac99f5
mARCIN <marcin.kozikowski.mk@gmail.com>
2021-06-10 14:01:38 +0200

    first base version with bfs algorythm
    first base version with bfs algorythm

---
+++ NonCrossingLinesDrawer/AdjacencyMatrix.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace NonCrossingLinesDrawer
+{
+    internal class AdjacencyMatrix
+    {
+        private readonly int _matrixSize;
+        private int _pointsCount;
+        private int[,] _matrix;
+
+        public int[,] Matrix => _matrix;
+        public int MatrixSize => _matrixSize;
+
+        public AdjacencyMatrix(int size)
+        {
+            _matrixSize = size;
+            _pointsCount = size * size;
+            _matrix = new int[_pointsCount, _pointsCount];
+            PrepareAdjacencyMatrix();
+        }
+
+        private void PrepareAdjacencyMatrix()
+        {
+            for (int i = 0; i < _pointsCount; i++)
+            {
+                SetNeighbours(i);
+            }
+        }
+
+        public void RemoveAdjacency(IEnumerable<int> points)
+        {
+            foreach (var point in points)
+            {
+                for (int i = 0; i < _pointsCount; i++)
+                {
+                    _matrix[point, i] = 0;
+                    _matrix[i, point] = 0;
+                }
+            }
+        }
+
+        private void SetNeighbours(int point)
+        {
+            //set left neighbour
+            if (point - 1 > 0 && (point % _matrixSize != 0))
+            {
+                _matrix[point, point - 1] = 1;
+                _matrix[point - 1, point] = 1;
+            }
+            //set right neighbour
+            if (point % _matrixSize != _matrixSize - 1)
+            {
+                _matrix[point, point + 1] = 1;
+                _matrix[point + 1, point] = 1;
+            }
+            //set top
+            if (point >= _matrixSize)
+            {
+                _matrix[point, point - _matrixSize] = 1;
+                _matrix[point - _matrixSize, point] = 1;
+            }
+            //set bottom
+            if (point < (_pointsCount - _matrixSize))
+            {
+                _matrix[point, point + _matrixSize] = 1;
+                _matrix[point + _matrixSize, point] = 1;
+            }
+        }
+    }
+
+}
---
+++ NonCrossingLinesDrawer/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace NonCrossingLinesDrawer
+{
+    public partial class MainWindow : Window
+    {
+        int selectedPoints = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for(int i=0;i<5;i++)
+            {
+                var row = new RowDefinition();
+
+                var column = new ColumnDefinition();
+
+                mainGrid.RowDefinitions.Add(row);
+                mainGrid.ColumnDefinitions.Add(column);
+
+                for (int j=0;j<5;j++)
+                {
+                    var canvas = new Canvas();
+                    canvas.Background = new SolidColorBrush(Colors.White);
+                    canvas.MouseEnter += Canvas_MouseEnter;
+                    canvas.MouseLeave += Canvas_MouseLeave;
+                    canvas.MouseDown += mainGrid_MouseDown;
+
+                    Grid.SetColumn(canvas, i);
+                    Grid.SetRow(canvas, j);
+
+                    mainGrid.Children.Add(canvas);
+                }
+            }
+
+            AdjacencyMatrix pixelsAdjacency = new AdjacencyMatrix(5);
+            var matrix = pixelsAdjacency.Matrix;
+
+            var bfs = new BFS(18, matrix);
+            bfs.getBFSPath();
+            var path = bfs.getBFSPathToPointR(6, new List<int>());
+            //List<int> path = bfs.getShortestBFSPath(6);
+            //textBlock.Text = pixelsAdjacency.PrintMatrixAsString();
+
+            //Po narysowaniu linni nalezy usunac wszelki powiazania do punktow ktore wchodza w sklad linni z matrixa
+            pixelsAdjacency.RemoveAdjacency(path);
+
+            var bfs2 = new BFS(4, pixelsAdjacency.Matrix);
+            bfs2.getBFSPath();
+            var path2 = bfs2.getBFSPathToPointR(16, new List<int>());
+
+            pixelsAdjacency.RemoveAdjacency(path2);
+
+            var bfs3 = new BFS(21, pixelsAdjacency.Matrix);
+            bfs3.getBFSPath();
+            var path3 = bfs3.getBFSPathToPointR(23, new List<int>());
+
+            pixelsAdjacency.RemoveAdjacency(path3);
+
+            var bfs4 = new BFS(9, pixelsAdjacency.Matrix);
+            bfs4.getBFSPath();
+            var path4 = bfs4.getBFSPathToPointR(15, new List<int>());
+            //List<int> path2 = bfs2.getShortestBFSPath(16);
+
+            //rysowanie kolejnej linni wymaga nowego przeszukania bfs z nowym matrixem
+        }
+
+        private void Canvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            (sender as Canvas).Background = new SolidColorBrush(Colors.White);
+        }
+
+        private void Canvas_MouseEnter(object sender, MouseEventArgs e)
+        {
+            (sender as Canvas).Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void mainGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+                Canvas _btn = sender as Canvas;
+
+                int _row = (int)_btn.GetValue(Grid.RowProperty);
+                int _column = (int)_btn.GetValue(Grid.ColumnProperty);
+
+                _btn.Background = new SolidColorBrush(Colors.LightGray);
+        }
+    }
+}
